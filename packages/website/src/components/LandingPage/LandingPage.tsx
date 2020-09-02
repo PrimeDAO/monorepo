175850fa430a0cab0f04122d563a586598ae6b6f
Doug Kent <github@douglaskent.com>
2020-09-02 15:02:38 -0400

    tweak

--- packages/website/src/components/LandingPage/LandingPage.tsx
+++ packages/website/src/components/LandingPage/LandingPage.tsx
@@ -21,51 +21,53 @@ const LandingPage = (): React.ReactElement => {
   return (
     <div className="landingPageWrapper">
       <div className="introduction">
-        <div className="leftColumn">
-          <div className="header">
-            <img className="logo" src="PrimeDAOLogo.svg"/>
-          </div>
-          <div className="body">
-            <div className="title">Introducing PrimeDAO:</div>
-            <div className="subtitle">An Adoption Engine for Open Finance</div>
-            <div className="body">A new system of open finance based on programmable money is being realized on Ethereum. PrimeDAO is here to catalyze and coordinate an open-source ecosystem of partners, builders, and users that simplifies, secures, and makes decentralized finance accessible (DeFi) for the masses.</div>
-          </div>
-          <div className="footer">
-            <div className="moreIcon"></div>
-          </div>
-        </div>
-        <div className="rightColumn" ref={menu}>
-          <div className="learn">
-            <div className="name"
-              onMouseEnter={() => showMenu('Learn', true)}
-              onMouseLeave={() => showMenu('Learn', false)}
-            >Learn<div className="menuDivider"></div></div>
-
-          </div>
-          <div className="connect">
-            <div className="name"
-              onMouseEnter={() => showMenu('Connect', true)}
-              onMouseLeave={() => showMenu('Connect', false)}
-            >Connect<div className="menuDivider"></div></div>
-          </div>
-          <div
-            onMouseEnter={() => showMenu('Learn', true)}
-            onMouseLeave={() => showMenu('Learn', false)}
-          >
-            <div className="learnMenu">
-              <div onClick={() => goto('https://medium.com/primedao')}><div className="name">Blog</div><div className="triangle"></div></div>
-              <div><div className="name">Litepaper</div><div className="triangle"></div></div>
+        <div className="container">
+          <div className="leftColumn">
+            <div className="header">
+              <img className="logo" src="PrimeDAOLogo.svg"/>
+            </div>
+            <div className="body">
+              <div className="title">Introducing PrimeDAO:</div>
+              <div className="subtitle">An Adoption Engine for Open Finance</div>
+              <div className="body">A new system of open finance based on programmable money is being realized on Ethereum. PrimeDAO is here to catalyze and coordinate an open-source ecosystem of partners, builders, and users that simplifies, secures, and makes decentralized finance accessible (DeFi) for the masses.</div>
+            </div>
+            <div className="footer">
+              <div className="moreIcon"></div>
             </div>
           </div>
-          <div
-            onMouseEnter={() => showMenu('Connect', true)}
-            onMouseLeave={() => showMenu('Connect', false)}
-          >
-            <div className="connectMenu">
-              <div onClick={() => goto('https://discord.gg/b8VjMfC')}><div className="name">Discord</div><div className="triangle"></div></div>
-              <div onClick={() => goto('https://twitter.com/PrimeDAO')}><div className="name">Twitter</div><div className="triangle"></div></div>
-              <div onClick={() => goto('https://github.com/PrimeDAO-Foundation')}><div className="name">Github</div><div className="triangle"></div></div>
-              <div onClick={() => goto('https://twitter.com/PrimeDAO')}><div className="name">Contact</div><div className="triangle"></div></div>
+          <div className="rightColumn" ref={menu}>
+            <div className="learn">
+              <div className="name"
+                onMouseEnter={() => showMenu('Learn', true)}
+                onMouseLeave={() => showMenu('Learn', false)}
+              >Learn<div className="menuDivider"></div></div>
+
+            </div>
+            <div className="connect">
+              <div className="name"
+                onMouseEnter={() => showMenu('Connect', true)}
+                onMouseLeave={() => showMenu('Connect', false)}
+              >Connect<div className="menuDivider"></div></div>
+            </div>
+            <div
+              onMouseEnter={() => showMenu('Learn', true)}
+              onMouseLeave={() => showMenu('Learn', false)}
+            >
+              <div className="learnMenu">
+                <div onClick={() => goto('https://medium.com/primedao')}><div className="name">Blog</div><div className="triangle"></div></div>
+                <div><div className="name">Litepaper</div><div className="triangle"></div></div>
+              </div>
+            </div>
+            <div
+              onMouseEnter={() => showMenu('Connect', true)}
+              onMouseLeave={() => showMenu('Connect', false)}
+            >
+              <div className="connectMenu">
+                <div onClick={() => goto('https://discord.gg/b8VjMfC')}><div className="name">Discord</div><div className="triangle"></div></div>
+                <div onClick={() => goto('https://twitter.com/PrimeDAO')}><div className="name">Twitter</div><div className="triangle"></div></div>
+                <div onClick={() => goto('https://github.com/PrimeDAO-Foundation')}><div className="name">Github</div><div className="triangle"></div></div>
+                <div onClick={() => goto('https://twitter.com/PrimeDAO')}><div className="name">Contact</div><div className="triangle"></div></div>
+              </div>
             </div>
           </div>
         </div>
