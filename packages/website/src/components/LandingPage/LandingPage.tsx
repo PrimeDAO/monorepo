1cbcdc97659049c2d77c852e61e8d0c3c4a1b1ca
Doug Kent <github@douglaskent.com>
2020-09-02 01:22:09 -0400

    more sections

--- packages/website/src/components/LandingPage/LandingPage.tsx
+++ packages/website/src/components/LandingPage/LandingPage.tsx
@@ -71,14 +71,35 @@ const LandingPage = (): React.ReactElement => {
         </div>
       </div>
       <div className="aboutUs">
-        <div className="title">About Us</div>
-        <div className="subtitle">Catalyzing DeFi Mass Adoption</div>
+        <div className="title">
+          <div className="triangle"></div>
+          <div className="name">About Us</div>
+          <div className="subtitle">Catalyzing DeFi Mass Adoption</div>
+        </div>
         <div className="subsections">
-          <div className="classname">
-            <div className="icon"></div>
+          <div className="section">
+            <div className="icon"><img src="PD_LP_ICON_01_02.jpg"/></div>
             <div className="title">The Decentralized Prime Brokerage</div>
             <div className="body">The PrimeDAO’s focus will be to deliver open finance support structures that a centralized prime broker would otherwise provide, such as efficient order matching, guaranteed settlement, liquidity for spot trades, lending, derivatives, leverage, and so on. Everybody should have easy access to prime brokerage tools and services that lets them lend and make markets. PrimeDAO will leverage transparent, decentralized governance to catalyze and democratize a new wave of DeFi adopters.</div>
           </div>
+          <div className="section">
+            <div className="icon"><img src="PD_LP_ICON_02_02.jpg" /></div>
+            <div className="title">Smart Aggregation</div>
+            <div className="body">On-chain liquidity aggregation is critical infrastructure for DeFi. By pooling liquidity sources, decentralized exchange aggregators reduce slippage and make large orders and liquidations easy to execute. PrimeDAO’s launch product — already fully developed — is an open-source smart router that aggregates liquidity from multiple DEXes and lending pools. Soon after launch, the smart router will be fully integrated with the DAO, whose members will govern its revenues and parameters.</div>
+          </div>
+          <div className="section">
+            <div className="icon"><img src="PD_LP_ICON_03_02.jpg" /></div>
+            <div className="title">DeFi Ecosystem Coordinator</div>
+            <div className="body">To preserve the DeFi commons’ fundamental ethos of permissionless access, it cannot be governed by a centralized entity. PrimeDAO aims to be a sustainable engine of value creation that earns and allocates resources towards a wide contributor ecosystem for promoting, simulating, building, auditing and maintaining the mechanisms and products of the DeFi ecosystem, cultivating projects that promote safety, reliability, liquidity, and above all, open access.</div>
+          </div>
+        </div>
+        <div className="ecosystem">
+          <div className="title">
+            <div className="triangle"></div>
+            <div className="name">Prime Ecosystem</div>
+            <div className="subtitle">PrimeDAO Visualized</div>
+          </div>
+          <div className="pic"><img src="CURVE_PD_DEFI_ECOSYSTEM_V02-1.jpg"/></div>
         </div>
       </div>
       <div className="footer">
