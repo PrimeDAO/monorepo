65b8d726435a11f0a479278210a2c35709deb090
arsenyjin <kinspotting@protonmail.com>
2020-09-25 15:59:49 +0200

    add migrate & set up crpool script

--- packages/contracts/migrations/2_deploy_contracts.js
+++ packages/contracts/migrations/2_deploy_contracts.js
@@ -4,6 +4,9 @@ const CRPFactory = artifacts.require('CRPFactory');
 const BFactory = artifacts.require('BFactory');
 const BalancerSafeMath = artifacts.require('BalancerSafeMath');
 const BalancerSafeMathMock = artifacts.require('BalancerSafeMathMock');
+const ConfigurableRightsPool = artifacts.require('ConfigurableRightsPool')
+const ERC20Mock = artifacts.require('ERC20Mock');
+const WETH = artifacts.require('WETH');
 
 module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(RightsManager);
@@ -18,4 +21,64 @@ module.exports = async function (deployer, network, accounts) {
     
     await deployer.deploy(CRPFactory);
 
+ 
+    const { toWei } = web3.utils
+    const MAX = web3.utils.toTwosComplement(-1)
+
+    const dai = await deployer.deploy(ERC20Mock, 'DAI Stablecoin', 'DAI', 18);
+    const weth = await deployer.deploy(WETH);
+  
+    await weth.deposit({ value: toWei('13.7') });
+
+    const tokenAddresses = [dai.address, weth.address];
+ 
+    const swapFee = 10 ** 15;
+    const startWeights = [toWei('5'), toWei('5')];
+    const startBalances = [toWei('10000'), toWei('13.7')];
+    const SYMBOL = 'BPOOL';
+    const NAME = 'Prime Balancer Pool Token';
+
+    const permissions = {
+          canPauseSwapping: true,
+          canChangeSwapFee: true,
+          canChangeWeights: true,
+          canAddRemoveTokens: true,
+          canWhitelistLPs: false,
+    };
+
+    const poolParams = {
+          poolTokenSymbol: SYMBOL,
+          poolTokenName: NAME,
+          constituentTokens: tokenAddresses,
+          tokenBalances: startBalances,
+          tokenWeights: startWeights,
+          swapFee: swapFee,
+    };
+
+    await console.log("   Creating 'Balancer' pool")
+    await console.log("   --------------------")
+
+    const crpFactory = await CRPFactory.deployed();
+    const bfactory = await BFactory.deployed();
+
+    POOL = await crpFactory.newCrp.call(
+            bfactory.address,
+            poolParams,
+            permissions,
+    );
+    
+    await crpFactory.newCrp(
+            bfactory.address,
+            poolParams,
+            permissions,
+    );
+
+    const pool = await ConfigurableRightsPool.at(POOL);
+
+    await dai.approve(POOL, MAX);
+    await weth.approve(POOL, MAX);
+
+    await pool.createPool(toWei('1000'));
+
+    await console.log('> contract address: ' + (pool.address).toString())
 };
