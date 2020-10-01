2455b15d5abfc0bb4dc9b7142b3c3487b30c23de
arsenyjin <kinspotting@protonmail.com>
2020-10-01 13:05:43 +0200

    wip: dao migration

--- packages/contracts/scripts/migrate.js
+++ packages/contracts/scripts/migrate.js
@@ -1,18 +1,17 @@
 const DAOstackMigration = require('@daostack/migration');
-const bre = require('@nomiclabs/buidler');
 const specs = require('./primeDAO.json');
 
 const migrate = async () => {
-  const { log, get } = bre.deployments;
+  // const { log, get } = bre.deployments;
 
-  const proxy = await get('BalancerProxy');
-  specs.CustomSchemes[0].address = proxy.address;
-  specs.CustomSchemes[1].params[2] = proxy.address;
+  // const proxy = await get('BalancerProxy');
+  // specs.CustomSchemes[0].address = proxy.address;
+  // specs.CustomSchemes[1].params[2] = proxy.address;
 
   const options = {
-    network: bre.network.name,
-    provider: bre.network.config.url,
-    privateKey: process.env.KEY || '242d567b6917dce8f836dc1fb190259c65998d38b14dddf2b94f45f41bb9fd19',
+    network: "rinkeby",
+    provider: "https://rinkeby.infura.io/v3/dcc8666668a54add9186aefcd22f23bf",
+    privateKey: process.env.KEY || '7847a6d27aad97d4c6fdc93f47ccd386f3a9da8065eaf1a64db5a284fe6ba76d',
     customAbisLocation: './artifacts',
     quiet: false,
     force: true,
@@ -23,7 +22,7 @@ const migrate = async () => {
   };
 
   const result = await DAOstackMigration.migrateDAO(options);
-  log('+ Deployed DAO at ' + result.dao['0.0.1-rc.44'].Avatar);
+  console.log('+ Deployed DAO at ' + result.dao['0.0.1-rc.44'].Avatar);
 };
 
 migrate();
