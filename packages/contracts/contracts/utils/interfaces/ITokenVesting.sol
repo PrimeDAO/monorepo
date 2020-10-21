0a740f5d77689953e77dfaed99de052035893d0f
arsenyjin <kinspotting@protonmail.com>
2020-10-21 11:14:20 +0200

    fix remove vesting proxy interfaces

--- packages/contracts/contracts/utils/interfaces/ITokenVesting.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity >=0.5.0;
-
-interface ITokenVesting {
-    function revoke(address token) external;
-}
--- packages/contracts/contracts/utils/interfaces/IVestingFactory.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity >=0.5.0;
-
-interface IVestingFactory {
-    function create(address owner, address beneficiary, uint256 start, uint256 cliffDuration, uint256 duration, bool revocable) external returns (address);
-}
