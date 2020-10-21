d3c75ace8d6350f8aa1c5f9707281044a3b404c6
Doug Kent <github@douglaskent.com>
2020-10-21 19:13:05 -0400

    more modalscreen

--- packages/interface/src/resources/customElements/modalscreen/modalscreen.ts
+++ packages/interface/src/resources/customElements/modalscreen/modalscreen.ts
@@ -1,6 +1,47 @@
 import "./modalscreen.scss";
-import { containerless, customElement } from "aurelia-framework";
+import { bindable, bindingMode, containerless, customElement } from "aurelia-framework";
 
+/**
+ * wrap this element around some content and set `onOff` to true when you want to disable and dim the contained content.
+ */
 @containerless
 @customElement("modalscreen")
-export class ModalScreen {}
+export class ModalScreen {
+
+  @bindable({ defaultBindingMode: bindingMode.toView }) public onOff: boolean;
+  private container: HTMLElement;
+  private mask: HTMLElement;
+
+  constructor() {
+    window.onresize = () => this.onResize();
+  }
+
+  private onOffChanged(newValue, oldValue) {
+    if (newValue && !oldValue) {
+      this.showHide(true);
+    } else if (!newValue && oldValue) {
+      this.showHide(false);
+    }
+  }
+
+  private onResize() {
+    if (this.container) {
+      this.mask.style.height = `${this.container.offsetHeight}px`;
+      this.mask.style.top = `${this.container.offsetTop}px`;
+    }
+  }
+
+  private showHide(onOff) {
+    if (this.mask) {
+      if (onOff) {
+        this.onResize();
+        this.mask.style.display = "flex";
+      } else {
+        this.mask.style.display = "none";
+      }
+    }
+  }
+  public attached(): void {
+    this.showHide(this.onOff);
+  }
+}
