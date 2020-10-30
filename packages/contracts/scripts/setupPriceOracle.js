d8ffa8d6969702199ddfda6527798db16729ba28
arsenyjin <kinspotting@protonmail.com>
2020-10-30 10:13:32 +0100

    add setupPriceOracle

---
+++ packages/contracts/scripts/setupPriceOracle.js
@@ -0,0 +1,24 @@
+const PrimeToken = artifacts.require('PrimeToken');
+const PriceOracle = artifacts.require('PriceOracle');
+
+
+const contracts = require('../contractAddresses.json');
+
+
+module.exports = async function(callback) {
+	const { toWei } = web3.utils;
+
+    const numerator = toWei('1');
+    const denominator = toWei('137'); 
+
+	const prime = contracts.kovan.PrimeToken;
+	const oracle = await PriceOracle.at(contracts.kovan.PriceOracle);
+
+	await console.log("***   Setting token price");
+
+	await oracle.setTokenPrice(prime, numerator, denominator);
+	
+	await console.log("***   Success");
+
+    callback();
+}
