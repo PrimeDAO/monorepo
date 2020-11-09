2a647d2644ab49e0fa04d7a535466aca13d3479b
arsenyjin <kinspotting@protonmail.com>
2020-11-09 14:48:15 +0100

    add mainnet migration

--- packages/contracts/migrations/2_deploy_contracts.js
+++ packages/contracts/migrations/2_deploy_contracts.js
@@ -7,6 +7,7 @@ const BalancerSafeMathMock = artifacts.require('BalancerSafeMathMock');
 const BalancerProxy = artifacts.require('BalancerProxy');
 const PrimeToken = artifacts.require('PrimeToken');
 const PriceOracle = artifacts.require('PriceOracle');
+const VestingFactory = artifacts.require('VestingFactory');
 
 const StakingRewards = artifacts.require('StakingRewards');
 
@@ -16,52 +17,36 @@ const fs = require("fs");
 module.exports = async function (deployer, network) {
     const { toWei } = web3.utils;
 
-    if (network === 'kovan') {
+    if (network === 'mainnet') {
 
-        await deployer.deploy(PrimeToken, primeSupply, primeSupply, deployer.networks.kovan.from);
         await deployer.deploy(StakingRewards);
         await deployer.deploy(PriceOracle);
         await deployer.deploy(BalancerProxy);
+        await deployer.deploy(VestingFactory);
 
-        contracts.kovan.PrimeToken = PrimeToken.address;
-        contracts.kovan.StakingRewards = StakingRewards.address;
-        contracts.kovan.PriceOracle = PriceOracle.address;
-        contracts.kovan.BalancerProxy = BalancerProxy.address;
+        contracts.mainnet.StakingRewards = StakingRewards.address;
+        contracts.mainnet.PriceOracle = PriceOracle.address;
+        contracts.mainnet.BalancerProxy = BalancerProxy.address;
+        contracts.mainnet.VestingFactory = VestingFactory.address;
 
         // overwrite contranctAddresses.json
         fs.writeFile('./contractAddresses.json', JSON.stringify(contracts), (err) => {
            if (err) throw err;
          });
 
-    }
-    else if (network === 'rinkeby') {
-        // deploy balancer configurable rights pool
-        await deployer.deploy(RightsManager);
-        await deployer.deploy(SmartPoolManager);
-        await deployer.deploy(BFactory);
-        await deployer.deploy(BalancerSafeMath);
-        await deployer.deploy(BalancerSafeMathMock);
-        await deployer.deploy(BalancerProxy);
-        await deployer.deploy(PriceOracle);
-
-        await deployer.link(BalancerSafeMath, CRPFactory);
-        await deployer.link(RightsManager, CRPFactory);
-        await deployer.link(SmartPoolManager, CRPFactory);
-
-        await deployer.deploy(CRPFactory);
-
-        // overwrite contrancts object
-        contracts.rinkeby.RightsManager = RightsManager.address;
-        contracts.rinkeby.SmartPoolManager = SmartPoolManager.address;
-        contracts.rinkeby.BFactory = BFactory.address;
-        contracts.rinkeby.BalancerSafeMath = BalancerSafeMath.address;
-        contracts.rinkeby.BalancerSafeMathMock = BalancerSafeMathMock.address;
-        contracts.rinkeby.BalancerProxy = BalancerProxy.address;
-        contracts.rinkeby.CRPFactory = CRPFactory.address;
-        contracts.rinkeby.PriceOracle = PriceOracle.address;
+    } else if (network === 'kovan') {
 
         await deployer.deploy(PrimeToken, primeSupply, primeSupply, deployer.networks.kovan.from);
         await deployer.deploy(StakingRewards);
+        await deployer.deploy(PriceOracle);
+        await deployer.deploy(BalancerProxy);
+        await deployer.deploy(VestingFactory);
+
+        contracts.kovan.PrimeToken = PrimeToken.address;
+        contracts.kovan.StakingRewards = StakingRewards.address;
+        contracts.kovan.PriceOracle = PriceOracle.address;
+        contracts.kovan.BalancerProxy = BalancerProxy.address;
+        contracts.kovan.VestingFactory = VestingFactory.address;
 
         // overwrite contranctAddresses.json
         fs.writeFile('./contractAddresses.json', JSON.stringify(contracts), (err) => {
