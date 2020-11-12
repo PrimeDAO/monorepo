a49ae1c98df690a1e97cb41eb5a10b5c7b76ed80
arsenyjin <kinspotting@protonmail.com>
2020-11-12 13:14:50 +0100

    wip add incentives deploy

--- packages/contracts/scripts/kovan/setupRewards.js
+++ packages/contracts/scripts/kovan/setupRewards.js
@@ -3,14 +3,15 @@ const StakingRewards = artifacts.require('StakingRewards');
 
 
 const contracts = require('../../contractAddresses.json');
+const config = require('../../config.json');
 
 
 module.exports = async function(callback) {
 	const { toWei } = web3.utils;
 
-    const yieldReward = toWei('500000');
-    const yieldStarTime = 1603411200; // 10/23/2020 @ 12:00am (UTC)
-    const yieldDuration = 7; // 7 days
+    const yieldReward = toWei(config.yieldRewards.reward);
+    const yieldStarTime = config.yieldRewards.startTime;
+    const yieldDuration = config.yieldRewards.duration; 
 
 	const prime = await PrimeToken.at(contracts.kovan.PrimeToken);
 	const staking = await StakingRewards.at(contracts.kovan.StakingRewards);
