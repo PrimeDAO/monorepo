464558af15bc68884e33872badfc469b03bcf790
arsenyjin <kinspotting@protonmail.com>
2020-11-12 17:27:59 +0100

    add innitRewards script

--- packages/contracts/scripts/mainnet/innitRewards.js
+++ packages/contracts/scripts/mainnet/innitRewards.js
@@ -1,24 +1,21 @@
-const PrimeToken = artifacts.require('PrimeToken');
 const StakingRewards = artifacts.require('StakingRewards');
 
-
 const contracts = require('../../contractAddresses.json');
-
+const config = require('../../config.json');
 
 module.exports = async function(callback) {
 	const { toWei } = web3.utils;
 
-    const yieldReward = toWei('500000');
-    const yieldStarTime = 1603411200; // 10/23/2020 @ 12:00am (UTC)
-    const yieldDuration = 7; // 7 days
+    const yieldReward = toWei(config.yieldRewards.reward);
+    const yieldStarTime = config.yieldRewards.startTime;
+    const yieldDuration = config.yieldRewards.duration; 
 
-	const prime = await PrimeToken.at(contracts.mainnet.PrimeToken);
 	const staking = await StakingRewards.at(contracts.mainnet.StakingRewards);
 
     try {
 
 		await console.log("***   Initializing StakingRewards");
-		await staking.initialize(prime.address, contracts.mainnet.ConfigurableRightsPool, yieldReward, yieldStarTime, yieldDuration);
+		await staking.initialize(contracts.mainnet.PrimeToken, contracts.mainnet.ConfigurableRightsPool, yieldReward, yieldStarTime, yieldDuration);
 		await console.log("***   Success");
 
     } catch(error) {
